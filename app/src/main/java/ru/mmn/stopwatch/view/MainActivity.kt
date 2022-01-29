587d89a5429023d74b16fae2cee6ad170125611c
Michael Makulov <mmn95@mail.ru>
2022-01-29 13:21:31 +0500

    -Добавлен 2-й таймер -Размеры шрифта и отступов вынесены в ресурсы

--- app/src/main/java/ru/mmn/stopwatch/view/MainActivity.kt
+++ app/src/main/java/ru/mmn/stopwatch/view/MainActivity.kt
@@ -18,14 +18,14 @@ class MainActivity : AppCompatActivity() {
     private val timestampProvider = TimestampProviderImpl()
 
     companion object {
-        const val TIMER = 1
+        const val TIMER_1 = 1
+        const val TIMER_2 = 2
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val textView = findViewById<TextView>(R.id.text_time)
         stopwatchModel = StopwatchViewModelFactory(
             stopwatchStateHolder = StopwatchStateHolder(
                 StopwatchStateCalculator(
@@ -38,18 +38,28 @@ class MainActivity : AppCompatActivity() {
 
         stopwatchModel.getLiveData().observe(this) {
             when (it.first) {
-                TIMER -> textView.text = it.second
+                TIMER_1 -> findViewById<TextView>(R.id.text_time_1).text = it.second
+                TIMER_2 -> findViewById<TextView>(R.id.text_time_2).text = it.second
             }
         }
 
-        findViewById<Button>(R.id.button_start).setOnClickListener {
-            stopwatchModel.start(TIMER)
+        findViewById<Button>(R.id.button_start_1).setOnClickListener {
+            stopwatchModel.start(TIMER_1)
         }
-        findViewById<Button>(R.id.button_pause).setOnClickListener {
-            stopwatchModel.pause(TIMER)
+        findViewById<Button>(R.id.button_pause_1).setOnClickListener {
+            stopwatchModel.pause(TIMER_1)
         }
-        findViewById<Button>(R.id.button_stop).setOnClickListener {
-            stopwatchModel.stop(TIMER)
+        findViewById<Button>(R.id.button_stop_1).setOnClickListener {
+            stopwatchModel.stop(TIMER_1)
+        }
+        findViewById<Button>(R.id.button_start_2).setOnClickListener {
+            stopwatchModel.start(TIMER_2)
+        }
+        findViewById<Button>(R.id.button_pause_2).setOnClickListener {
+            stopwatchModel.pause(TIMER_2)
+        }
+        findViewById<Button>(R.id.button_stop_2).setOnClickListener {
+            stopwatchModel.stop(TIMER_2)
         }
 
     }
